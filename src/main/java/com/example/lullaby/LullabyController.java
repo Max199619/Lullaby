834f3b7e84d7b8c14df0122a683b30fcf867bc12
Max199619 <66778549+Max199619@users.noreply.github.com>
2024-01-20 22:11:24 +0100

    working program

--- src/main/java/com/example/lullaby/LullabyController.java
+++ src/main/java/com/example/lullaby/LullabyController.java
@@ -1,10 +1,14 @@
 package com.example.lullaby;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static java.lang.System.exit;
 
 public class LullabyController {
 
@@ -16,21 +20,64 @@ public class LullabyController {
     public Button hibernateButton;
     public Button rebootButton;
     public Button shutdownButton;
+    public int counter;
+    Timer timer = new Timer();
+    Runtime run = Runtime.getRuntime();
 
     public void GoToSleep(ActionEvent event) {
+        calculateSeconds();
         switchOffTextfields();
+        while (counter > 0){
+            timer.scheduleAtFixedRate(tick, 0, 1000);
+        }
+        try {
+            run.exec("RUNDLL32.EXE powrprof.dll,SetSuspendState 0,1,0");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        exit(0);
     }
 
     public void Hibernate(ActionEvent event) {
+        calculateSeconds();
         switchOffTextfields();
+        while (counter > 0){
+            timer.scheduleAtFixedRate(tick, 0, 1000);
+        }
+        try {
+            run.exec("shutdown/h /t 0");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        exit(0);
     }
 
     public void Restart(ActionEvent event) {
+        calculateSeconds();
         switchOffTextfields();
+        while (counter > 0){
+            timer.scheduleAtFixedRate(tick, 0, 1000);
+        }
+        try {
+            run.exec("shutdown /r");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        exit(0);
     }
 
     public void SwitchOff(ActionEvent event) {
+        calculateSeconds();
         switchOffTextfields();
+        while (counter > 0){
+            timer.scheduleAtFixedRate(tick, 0, 1000);
+        }
+        try {
+            run.exec("shutdown /s");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        exit(0);
     }
 
     private void switchOffTextfields(){
@@ -38,4 +85,22 @@ public class LullabyController {
         minutes.setDisable(true);
         seconds.setDisable(true);
     }
+
+    TimerTask tick = new TimerTask() {
+        @Override
+        public void run() {
+            hours.setText(String.valueOf(counter / 3600));
+            minutes.setText(String.valueOf((counter % 3600) /60));
+            seconds.setText(String.valueOf((counter % 3600) % 60));
+            counter = counter - 1;
+        }
+    };
+
+
+    private void calculateSeconds(){
+        counter += Integer.parseInt(seconds.getText());
+        counter += Integer.parseInt(minutes.getText()) * 60;
+        counter += Integer.parseInt(hours.getText()) * 60 * 60;
+    }
+
 }
